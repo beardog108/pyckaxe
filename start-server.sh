ddf8914e2879be68d71c1f77d84a9d38c7b12604
Kevin Froman <beardog108@users.noreply.github.com>
2017-10-08 01:12:57 -0500

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+if [ ! -f mc.jar ]; then
+    echo "There is not a file named mc.jar in this directory. Exiting.";
+    exit 1;
+fi
+
+if ! [ -x "$(command -v tmux)" ]; then
+  echo 'Error: tmux is not installed.' >&2
+  exit 1;
+fi
+
 tmux new-session -P -d -n minecraft-pyckaxe "java -jar mc.jar nogui > mc.log"> tmuxid.txt
